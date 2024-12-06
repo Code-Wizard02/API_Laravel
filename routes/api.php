a854c60db02c7c02ec3a20a9452a64ef47df5ec0
Kevin Hernández <21160665@itoaxaca.edu.mx>
2024-12-05 18:58:54 -0600

    Adding Products API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -21,3 +22,5 @@ Route::group(
         Route::get('userResourceCollectionWithMeta', [AuthController::class, 'userResourceCollectionWithMeta']);
     }
 );
+
+Route::apiResource('products', ProductController::class);
